11676ed1216e8dfc6f2844333cb94afb7a63d3a6
Christian Zenaty <cazenaty@yahoo.com>
2018-03-20 15:00:41 -0700

    heroku deploy fix

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const WebSocket = require("ws");
 const http = require("http");
-const bodyParser = require("body-parser");
+const path = require("path");
 
 const app = express();
 const server = http.createServer(app);
